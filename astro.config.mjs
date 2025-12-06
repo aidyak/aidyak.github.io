e3e1be3ecbd1bfecba7e34f406bc98620b165e8e
aidyak <yusei.kaida@giftee.co>
2025-12-06 11:15:01 +0900

    fix some config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aidyak.github.io',
-  base: '/aidyak.github.io',
+  // For user pages (aidyak.github.io), base should be root
+  base: '/',
 });
